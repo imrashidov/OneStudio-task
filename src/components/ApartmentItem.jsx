efdb39d55eff79c04472d8f9eae7a393cf2eded8
Farid Rashidov <frashidov@gmail.com>
2024-04-27 20:22:08 +0400

    Refactor CSS styles in Apartments and Services components, and update Apartments and Services components

--- src/components/ApartmentItem.jsx
+++ src/components/ApartmentItem.jsx
@@ -4,9 +4,44 @@ import data from "../data/data";
 const ApartmentItem = ({ handleItemClick, active }) => {
   return (
     <div className="apt-items">
-      {(active === "b" ? data.aptItemsB : data.aptItemsA).map((item) => (
+      {(active === "b"
+        ? data.aptItemsB.slice(0, 4)
+        : data.aptItemsA.slice(0.4)
+      ).map((item) => (
         <div
           className="apt-item"
+          key={item.id}
+          onClick={() => handleItemClick(item)}
+        >
+          {item.sold === true && <span>Satılıb</span>}
+          <div className="apt-image">
+            <img src={item.img} alt="Apartment" />
+            <h5>
+              {item.room} - {item.area}
+            </h5>
+          </div>
+          <div className="apt-details">
+            <div className="apt-detail">
+              <img src={item.aptIcon} alt="Apartment" />
+              <p>{item.apartment}</p>
+            </div>
+            <div className="apt-detail">
+              <img src={item.roomIcon} alt="Apartment" />
+              <p>{item.room}</p>
+            </div>
+            <div className="apt-detail">
+              <img src={item.areaIcon} alt="Apartment" />
+              <p>{item.area}</p>
+            </div>
+          </div>
+        </div>
+      ))}
+      {(active === "b"
+        ? data.aptItemsB.slice(4, 8)
+        : data.aptItemsA.slice(4, 8)
+      ).map((item) => (
+        <div
+          className="apt-item-2"
           key={item.id}
           onClick={() => handleItemClick(item)}
         >
